99a7304b364ec5cab6740876bf228bdcc1c8bb35
Gene Pressinger <genepressinger@yahoo.com>
2021-04-21 15:42:05 -0400

    Added Thomas' new changes

--- src/accounts/User.java
+++ src/accounts/User.java
@@ -2,106 +2,170 @@ package accounts;
 
 import java.util.*;
 
-public class User {
+public class User implements AccountInterface{
 
     Scanner in = new Scanner(System.in);
 
-    public static int ACTIVE_ACCOUNT = 1;
-    String Username;
-    String Password;
-    String Name;
-    String Email;
-    String PhoneNumber;
-    String Birthday;
-    String Gender;
-    String Location;
-    int Status;
+    protected static String name;
+    protected static String email;
+    protected static String phoneNumber;
+    protected static String birthday;
+    protected static String gender;
+    protected static String location;
+    public static final int INACTIVE_ACCOUNT = -1;
+    public static final int ACTIVE_ACCOUNT = 1;
+    protected static String username;
+    protected static String password;
+    protected static int status;
 
-    public void setUserName(String _username) {
-        Username = _username;
+    public User(String _name,String _email,String _phoneNumber,String _birthday, String _gender,String _location,String _username,String _password){
+        this.name = _name;
+        this.email = _email;
+        this.phoneNumber = _phoneNumber;
+        this.birthday = _birthday;
+        this.gender = _gender;
+        this.location = _location;
+        this.username = _username;
+        this.password = _password;
+        this.status = ACTIVE_ACCOUNT;
     }
 
-    public String getUserName() {
-        return Username;
-    }
-
-    public void setPassword(String _password) {
-        Password = _password;
-    }
-
-    public String getPassword(){
-        return Password;
-    }
-
-    public void setName(String _name) {
-       Name = _name;
-    }
+    //=================  GETTERS ===============
 
     public String getName(){
-        return Name;
-    }
-
-    public void setEmail(String _email) {
-        Email = _email;
+        return name;
     }
 
     public String getEmail(){
-        return Email;
-    }
-
-    public void setPhoneNumber(String _phonenumber) {
-        PhoneNumber = _phonenumber;
+        return email;
     }
 
     public String getPhoneNumber(){
-        return PhoneNumber;
-    }
-
-    public void setBirthday(String _birthday) {
-        Birthday = _birthday;
+        return phoneNumber;
     }
 
     public String getBirthday(){
-        return Birthday;
-    }
-
-    public void setGender(String _gender) {
-        Gender = _gender;
+        return birthday;
     }
 
     public String getGender(){
-        return Gender;
-    }
-
-    public void setLocation(String _location) {
-        Location = _location;
+        return gender;
     }
 
     public String getLocation(){
-        return Location.toUpperCase();
+        return location.toUpperCase();
     }
 
-    public void setStatus(int _status){
-        Status = _status;
+    public String getUserName() {
+        return username;
     }
 
-    public void setStatus(){
-        Status = ACTIVE_ACCOUNT;
+    public String getPassword(){
+        return password;
     }
 
     public int getStatus(){
-        return Status;
+        return status;
+    }
+
+    //=================  SETTERS ===============
+
+    public void setName(String _name) {
+       name = _name;
+    }
+
+    public void setEmail(String _email) {
+        email = _email;
+    }
+
+    public void setPhoneNumber(String _phoneNumber) {
+        phoneNumber = _phoneNumber;
+    }
+
+    public void setBirthday(String _birthday) {
+        birthday = _birthday;
+    }
+
+    public void setGender(String _gender) {
+        gender = _gender;
+    }
+
+    public void setLocation(String _location) {
+        location = _location;
+    }
+
+    public void setUserName(String _username) {
+        username = _username;
+    }
+
+    public void setPassword(String _password) {
+        password = _password;
+    }
+
+    public void setStatus(){
+        status = ACTIVE_ACCOUNT;
+    }
+
+    public void resetStatus(int _status){
+        status = _status;
+    }
+
+    public String inputUserName(){
+        System.out.println("What is your Username");
+        return in.nextLine().toLowerCase();
+    }
+
+    public String inputPassword(){
+        System.out.println("What is your password");
+        return in.nextLine();
+    }
+
+    //Next part needs to search through database for usernames.
+    // Checks if one is there
+    // Get information from the database
+    //DOESNT WORK NOW
+    private User searchAccount() throws InactiveAccountException, InvalidPasswordException {
+        String username = inputUserName();
+        //finds account
+        User account = login(); //Just used to make this compile. Not useful as of now
+        if (account.getStatus() == INACTIVE_ACCOUNT) {
+            throw new InactiveAccountException("Account doesn't exist");
+        }
+        if (account.getUserName().equals(username)) {
+            return account;
+        }
+        throw new InactiveAccountException("Can't find account");
+    }
+
+    public User login() throws InvalidPasswordException, InactiveAccountException {
+        User account = searchAccount();
+        String password = inputPassword();
+        if(account.getPassword().equals(password)) {
+            throw new InvalidPasswordException("Incorrect Password");
+        } else if(account.getStatus() == INACTIVE_ACCOUNT) {
+            throw new InactiveAccountException("Account does not exist");
+        }
+        else{
+            return account;
+        }
+    }
+
+    public void deleteAccount() throws InactiveAccountException, InvalidPasswordException {
+        User account = login();
+        account.resetStatus(INACTIVE_ACCOUNT);
+        System.out.println("Account Deleted");
     }
 
     @Override
     public String toString() {
-        return Username + ":" + " , " +
-                Name  + ":" + " , " +
-                Email + ":" + " , " +
-                PhoneNumber + ":" + " , " +
-                Birthday + ":" + " , " +
-                Gender + ":" + " , " +
-                Location + ":" + " , " +
-                Status;
+        return  username + ":" + " , " +
+                name  + ":" + " , " +
+                email + ":" + " , " +
+                phoneNumber + ":" + " , " +
+                birthday + ":" + " , " +
+                gender + ":" + " , " +
+                location  + ":" + " , " +
+                status;
     }
+
 }
--- src/api/Main.java
+++ src/api/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) throws JSONException, NotDirectFlightException {
 String departureDate = "2021-06-03";
 String originAirport = "LAX";
-	GetFlightData test = new GetFlightData();
-        System.out.println("Carrier " + test.getCarrier(departureDate,originAirport));
-        System.out.println("Which quote is direct? " + test.isDirectFlight(departureDate,originAirport));
-        System.out.println("Price " + test.getPrice(departureDate,originAirport));
-        System.out.println("Carrier ID " + test.getCarrierId(departureDate,originAirport));
-        System.out.println("Is Direct Flight: " + test.isDirectFlight(departureDate,originAirport));
+	GetFlightData test = new GetFlightData(departureDate,originAirport);
+        System.out.println("Carrier " + test.getCarrier());
+        System.out.println("Which quote is direct? " + test.isDirectFlight());
+        System.out.println("Price " + test.getPrice());
+        System.out.println("Carrier ID " + test.getCarrierId());
+        System.out.println("Is Direct Flight: " + test.isDirectFlight());
 
 
 	//test.getQuote("2019-09-01","SDF");
