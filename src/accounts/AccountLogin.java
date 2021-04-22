3bd498cc577da6d28203fae5e5ff3c7a29039fb3
lww1117 <lww1117@gmail.com>
2021-04-22 02:36:30 -0400

    this was tonights work

--- src/accounts/AccountLogin.java
+++ src/accounts/AccountLogin.java
@@ -2,8 +2,9 @@ package accounts;
 
 public class AccountLogin {
     public static boolean credentialsAreValid(String username, String password) {
-        // TODO: Implement this using database connection
-        return true;
+        String user = db.QuereyFunk.getUsernameFromPassword(password);
+        String pass = db.QuereyFunk.getPasswordFromUsername(username);
+        return user.equals(pass);
     }
 
     public static void createAccount(String username, String password, String email) {
--- src/db/Main.java
+++ src/db/Main.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         ConnectToDB.testConnect();
+
+
     }
 
 }
