877401d753b29cbeeef0c88990f4bd2f9420153a
Tknep3 <75146891+Tknep3@users.noreply.github.com>
2021-04-28 14:51:19 -0400

    Delete NotDirectFlightException.java

--- NotDirectFlightException.java
+++
@@ -1,8 +0,0 @@
-package api;
-
-//Signifies a FLight is not Direct
-public class NotDirectFlightException extends Exception {
-    public NotDirectFlightException(String message){
-        super(message);
-    }
-}
