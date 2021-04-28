0c608d81a2441206dbfa4280563ae2c047668449
Tknep3 <75146891+Tknep3@users.noreply.github.com>
2021-04-28 14:52:02 -0400

    Delete InactiveAccountException.java

--- InactiveAccountException.java
+++
@@ -1,8 +0,0 @@
-package accounts;
-
-//Signifies an account does not exist or is marked INACTIVE.
-public class InactiveAccountException extends Exception{
-    public InactiveAccountException(String message){
-        super(message);
-    }
-}
