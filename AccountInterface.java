d9deb583d9bc670d59e017ef445c77ba3c393381
Tknep3 <75146891+Tknep3@users.noreply.github.com>
2021-04-30 14:46:21 -0400

    Add files via upload

---
+++ AccountInterface.java
@@ -0,0 +1,16 @@
+package accounts;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface AccountInterface {
+
+    public User login() throws InvalidPasswordException, InactiveAccountException;
+
+    void deleteAccount() throws InactiveAccountException, InvalidPasswordException;
+
+    private User searchAccount() throws InactiveAccountException, InvalidPasswordException {
+        return null;
+    }
+
+}
